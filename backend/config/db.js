3a351fd3d56c07080d5173f177b54d2d3387d69f
stanleygarbo <stanleygarbo@gmail.com>
2024-09-15 17:02:14 +0800

    db config setup

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+// config/config.js
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); // Exit the app if unable to connect
+  }
+};
+
+module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 const port = 3000;
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({
