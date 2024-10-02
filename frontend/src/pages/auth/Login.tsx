be892d09629d74d7a5bdc6c0a597a035b64da4c1
stanleygarbo <stanleygarbo@gmail.com>
2024-10-02 14:32:52 +0800

    added rest client

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,5 +1,13 @@
 const Login = () => {
-  return <div className="">Login</div>;
+  return (
+    <div className="">
+      <form action="" className="bg-blue-500 flex flex-col">
+        <input className="" type="text" placeholder="Username" />
+        <input type="text" placeholder="Password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
