94fe61c26f5deebda4ff433ed189182fc22a911a
stanleygarbo <stanleygarbo@gmail.com>
2024-10-03 16:52:02 +0800

    revised design

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,31 +2,32 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row-reverse gap-4 bg-white pt-5 pb-5 m-auto border-b">
-      <div className="flex gap-x-1 justify-between w-full px-5">
+    <div className="fixed top-0 left-0 flex z-50 w-full flex-row-reverse gap-4 bg-white pt-5 pb-5 m-auto border-b">
+      <div className="flex gap-x-1 justify-between items-center w-full px-10">
         <div className="flex items-start pr-2 pl-2">
           <Link className=" flex text-xs font-bold items-center" to="/">
             <img
               src="./src/Pictures/images.png"
               alt="ACLC"
-              className="size-[2rem] rounded-full border-2 border-red-500"
+              className="size-11 rounded-full border-2 border-red-500"
             />
             <p className="ml-3 text-sm font-black">ACLC COLLEGE</p>
+            <span className="text-2xl ml-2">🌿</span>
           </Link>
         </div>
         <section className="flex">
           <div className="pr-2 pl-2 hover:text-red-500 transition-all hover:scale-110">
-            <Link className="text-xs font-bold  " to="/">
+            <Link className="text-base font-bold  " to="/">
               Home
             </Link>
           </div>
           <div className="pr-2 pl-2 hover:text-red-500 transition-all hover:scale-110">
-            <Link className="text-xs font-bold" to="/about">
+            <Link className="text-base font-bold" to="/about">
               About
             </Link>
           </div>
           <div className="pr-2 pl-2 hover:text-red-500 transition-all hover:scale-110">
-            <Link className="text-xs font-bold" to="/login">
+            <Link className="text-base font-bold" to="/login">
               Login
             </Link>
           </div>
