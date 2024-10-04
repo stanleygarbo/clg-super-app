4cdd67fe096cb1c6426aba9335b3036abacac696
stanleygarbo <stanleygarbo@gmail.com>
2024-10-04 16:15:56 +0800

    added styles for larger screens

--- frontend/src/components/home/prevHome.tsx
+++ frontend/src/components/home/prevHome.tsx
@@ -35,31 +35,37 @@ const PrevHome = () => {
 
   return (
     <div className="">
-      <section className="w-full h-screen border-b flex">
-        <div className="w-full pl-[170px] z-20 mt-[241px] ">
-          <h1 className="text-[100px] font-bold leading-[100px]">
-            ACLC College <br /> of{" "}
-            <span className="relative">
-              Ormoc
-              <img src="/line.svg" className=" absolute left-0 w-full" alt="" />
-            </span>
-          </h1>
-          {/* <div className="h-[6px] w-[140px] rounded gradient1"></div>
+      <section className="w-full h-screen border-b flex items-center">
+        <div className="w-full pl-[170px] z-20 ">
+          <div className="max-w-[1800px] mx-auto">
+            <h1 className="text-[100px] font-bold leading-[100px]">
+              ACLC College <br /> of{" "}
+              <span className="relative">
+                Ormoc
+                <img
+                  src="/line.svg"
+                  className=" absolute left-0 w-full"
+                  alt=""
+                />
+              </span>
+            </h1>
+            {/* <div className="h-[6px] w-[140px] rounded gradient1"></div>
           <div className="ml-[120px] mt-2 h-[6px] w-[60px] rounded gradient1"></div> */}
 
-          <p className="w-[400px] my-14 text-lg">
-            A leading computer training institution in the country offering full
-            2-year programs and short-term courses
-          </p>
-          <button className="w-64 bg-black border border-black text-white py-4 uppercase rounded-md font-bold text-sm hover:bg-white hover:text-black hover:border-black transition-colors duration-200">
-            Contact us
-          </button>
+            <p className="w-[400px] my-14 text-lg">
+              A leading computer training institution in the country offering
+              full 2-year programs and short-term courses
+            </p>
+            <button className="w-64 bg-black border border-black text-white py-4 uppercase rounded-md font-bold text-sm hover:bg-white hover:text-black hover:border-black transition-colors duration-200">
+              Contact us
+            </button>
+          </div>
         </div>
         <div className="absolute w-[60%] z-10 right-0 h-screen bg-[url('/aclc-bldg.jpg')] bg-center bg-cover opacity-70">
           <div className="absolute w-full h-full bg-white opacity-30"></div>
         </div>
       </section>
-      <div className="relative w-full m-auto grid grid-cols-2">
+      <div className="relative max-w-[1600px] m-auto grid grid-cols-2">
         <div className="pb-5 pt-20 relative">
           <h1 className="text-3xl  font-bold flex items-center">
             <div className="w-32 h-1 bg-black mr-4"></div>
