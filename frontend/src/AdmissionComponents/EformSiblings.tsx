814f10acf29bf332d6dce98a2ee8fd8e22dc4fda
Techno <meroypaulvincent@gmail.com>
2024-10-06 12:57:05 +0800

    fixed form

--- frontend/src/AdmissionComponents/EformSiblings.tsx
+++ frontend/src/AdmissionComponents/EformSiblings.tsx
@@ -1,49 +1,45 @@
 import React from "react";
 
 const EformSiblings = () => {
-  return (
-    <div>
-      <div className="p-10 pt-3">
-        <form action="" className="pb-4 border-4 flex flex-col border-black ">
-          <div className="bg-slate-900 text-white text-center pb-5 pt-5 border-b-4 border-black w-[100%]">
-            <p>SIBLING'S INFORMATION</p>
-          </div>
-          <div className="flex flex-row gap-[10px] py-3 px-[5rem] ">
-            <div className="flex flex-col p-5 gap-3 w-[700px]  ">
-              <label htmlFor="" className="text-center">
-                BROTHERS/SISTERS' NAME
-              </label>
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
+    return (
+        <form
+            action=""
+            className="my-10 pb-4 border-4 flex flex-col border-black "
+        >
+            <div className="bg-slate-900 text-white text-center pb-5 pt-5 border-b-4 border-black w-[100%]">
+                <p>SIBLING'S INFORMATION</p>
             </div>
-            <div className="flex flex-col p-5 gap-3 w-[150px]">
-              <label htmlFor="" className="text-center">
-                AGE
-              </label>
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
+            <div
+                className="grid gap-[10px] py-3 px-5"
+                style={{ gridTemplateColumns: "1fr 100px 1fr" }}
+            >
+                <label htmlFor="" className="text-center">
+                    BROTHERS/SISTERS' NAME
+                </label>
+                <label htmlFor="" className="text-center">
+                    AGE
+                </label>
+                <label htmlFor="" className="text-center">
+                    SCHOOL/OCCUPATION
+                </label>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
             </div>
-            <div className="flex flex-col p-5 gap-3 w-[700px]">
-              <label htmlFor="" className="text-center">
-                SCHOOL/OCCUPATION
-              </label>
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-            </div>
-          </div>
         </form>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default EformSiblings;
