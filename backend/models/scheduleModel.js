a9ba98b1e55bbf473a6483f7012c34673f0eab4e
TechnoDev <74283450+TechnoX0@users.noreply.github.com>
2025-02-13 20:34:17 +0800

    created days enum and schedule model, and modified user model to include schedule

---
+++ backend/models/scheduleModel.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import days from "../utils/days";
+
+const subjectScheduleSchema = new mongoose.Schema(
+    {
+        courseID: {
+            type: String,
+            required: true,
+        },
+        timeStart: {
+            type: String,
+            required: true,
+        },
+        timeEnd: {
+            type: String,
+            required: true,
+        },
+        day: {
+            type: String,
+            enum: days,
+            required: true,
+        },
+        room: {
+            type: String,
+            required: true,
+        },
+        instructorID: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const scheduleSchema = new mongoose.Schema(
+    {
+        schoolYear: {
+            type: Date,
+            required: true,
+        },
+        semester: {
+            type: String,
+            enum: ["1st", "2nd", "summer"],
+            required: true,
+        },
+        subjectSchedules: {
+            type: [subjectScheduleSchema],
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const Schedule = mongoose.model("Schedule", scheduleSchema);
+const SubjectSchedule = mongoose.model(
+    "SubjectSchedule",
+    subjectScheduleSchema
+);
+
+module.exports = { Schedule, SubjectSchedule };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,101 +8,103 @@ const { birthSchema } = require("./birthModel");
 const { cityAddressSchema, homeAddressSchema } = require("./addressModel");
 const { siblingSchema } = require("./siblingModel");
 const standing = require("../constants/studentStanding");
+import scheduleSchema from "./scheduleModel";
 
 const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: false,
-      unique: true,
+    {
+        username: {
+            type: String,
+            required: false,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: false,
+            select: false,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        surname: {
+            type: String,
+            required: true,
+        },
+        middleName: {
+            type: String,
+        },
+        birthDate: {
+            type: Date,
+            required: false,
+        },
+        email: {
+            type: String,
+            required: false,
+        },
+        telephone: {
+            type: String,
+            required: false,
+        },
+        phone: {
+            type: String,
+            required: false,
+        },
+        roles: {
+            type: [String],
+            enum: roles,
+            required: true,
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+            select: false,
+        },
+        spouse: spouseSchema,
     },
-    password: {
-      type: String,
-      required: false,
-      select: false,
-    },
-    firstName: {
-      type: String,
-      required: true,
-    },
-    surname: {
-      type: String,
-      required: true,
-    },
-    middleName: {
-      type: String,
-    },
-    birthDate: {
-      type: Date,
-      required: false,
-    },
-    email: {
-      type: String,
-      required: false,
-    },
-    telephone: {
-      type: String,
-      required: false,
-    },
-    phone: {
-      type: String,
-      required: false,
-    },
-    roles: {
-      type: [String],
-      enum: roles,
-      required: true,
-    },
-    isDeleted: {
-      type: Boolean,
-      default: false,
-      select: false,
-    },
-    spouse: spouseSchema,
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
 
 const studentSchema = new mongoose.Schema(
-  {
-    program: { type: mongoose.Schema.Types.ObjectId, ref: "Program" },
-    standing: {
-      type: String,
-      enum: standing,
-      default: "freshman",
+    {
+        program: { type: mongoose.Schema.Types.ObjectId, ref: "Program" },
+        standing: {
+            type: String,
+            enum: standing,
+            default: "freshman",
+        },
+        birth: birthSchema,
+        homeAddress: homeAddressSchema,
+        cityAddress: cityAddressSchema,
+        father: parentSchema,
+        mother: parentSchema,
+        guardian: guardianSchema,
+        guardianSpouse: spouseSchema,
+        siblings: [siblingSchema],
+        schedules: scheduleSchema,
     },
-    birth: birthSchema,
-    homeAddress: homeAddressSchema,
-    cityAddress: cityAddressSchema,
-    father: parentSchema,
-    mother: parentSchema,
-    guardian: guardianSchema,
-    guardianSpouse: spouseSchema,
-    siblings: [siblingSchema],
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 const employeeSchema = new mongoose.Schema(
-  {
-    sickLeave: { type: Number, default: 0 }, // Total leave days allotted
-    vacationLeave: { type: Number, default: 0 }, // Total vacation days allotted
-    hireDate: { type: Date, required: true },
-    employmentType: {
-      type: String,
-      required: true,
-      enum: employmentType,
+    {
+        sickLeave: { type: Number, default: 0 }, // Total leave days allotted
+        vacationLeave: { type: Number, default: 0 }, // Total vacation days allotted
+        hireDate: { type: Date, required: true },
+        employmentType: {
+            type: String,
+            required: true,
+            enum: employmentType,
+        },
+        department: { type: mongoose.Schema.Types.ObjectId, ref: "Department" },
+        position: { type: mongoose.Schema.Types.ObjectId, ref: "Position" },
+        governmentId: GovernmentIdSchema,
+        birth: birthSchema,
+        homeAddress: homeAddressSchema,
+        cityAddress: cityAddressSchema,
     },
-    department: { type: mongoose.Schema.Types.ObjectId, ref: "Department" },
-    position: { type: mongoose.Schema.Types.ObjectId, ref: "Position" },
-    governmentId: GovernmentIdSchema,
-    birth: birthSchema,
-    homeAddress: homeAddressSchema,
-    cityAddress: cityAddressSchema,
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 const Employee = User.discriminator("Employee", employeeSchema);
