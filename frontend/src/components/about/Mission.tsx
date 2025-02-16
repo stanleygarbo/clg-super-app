86e42c4c21032b447a6188ff9c3a190dacec8fef
markjasperrojas <markjasperrojas1@gmail.com>
2025-02-16 20:54:17 +0800

    fixing ui in about

--- frontend/src/components/about/Mission.tsx
+++ frontend/src/components/about/Mission.tsx
@@ -1,33 +1,18 @@
 const Mission = () => {
   return (
-    <div className="flex">
-      <div className="flex items-center">
-        <article>
-          <h1 className="text-center font-bold text-3xl"> MISSION </h1>
-          <p className="text-base font-semibold">
-            To provide a holistic, relevant, quality and globally recognized
-            IT-based education in all levels and disciplines with the objective
-            of producing professionals and leaders responsive to the needs of
-            Science and the international community cognizant of the welfare and
-            benefits of its men and women thereby realizing their potential as
-            productive members of the society for the honor and glory of God
-            Almighty.
-          </p>
-        </article>
-      </div>
-      {/* <div className="mt-20">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/0eaqOCzGEh4?si=p6OqhLqTIZ6033mb"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-          className="m-10 border-2 border-black"
-        ></iframe>
-      </div> */}
+    <div className="max-w-4xl mx-auto px-4 py-16 bg-gray-50">
+      <article className="text-center">
+        <h1 className="text-3xl font-bold mb-6">Mission</h1>
+        <p className="text-lg text-gray-700 leading-relaxed">
+          To provide a holistic, relevant, quality and globally recognized
+          IT-based education in all levels and disciplines with the objective
+          of producing professionals and leaders responsive to the needs of
+          Science and the international community cognizant of the welfare and
+          benefits of its men and women thereby realizing their potential as
+          productive members of the society for the honor and glory of God
+          Almighty.
+        </p>
+      </article>
     </div>
   );
 };
