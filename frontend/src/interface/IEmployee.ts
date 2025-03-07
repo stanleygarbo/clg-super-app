ecd01e3fa676539a648b68449f2cb802b6600936
cripe101 <mhegryanlimpangog58@gmail.com>
2025-03-07 19:35:02 +0800

    Updated

--- frontend/src/interface/IEmployee.ts
+++ frontend/src/interface/IEmployee.ts
@@ -2,35 +2,35 @@ import { IDepartmentGet } from "./IDepartment";
 import { IPositionGet } from "./IPosition";
 
 export interface IEmployeeGet {
-    includes: any;
-    toLowerCase(): unknown;
-    _id: string;
-    firstName: string;
-    surname: string;
-    middleName: string;
-    username: string;
-    password: string;
-    department: IDepartmentGet;
-    position: IPositionGet;
-    hireDate: string;
-    employmentType: string;
-    roles: string[];
-    address: object;
-    gender: string;
+  includes: any;
+  toLowerCase(): unknown;
+  _id: string;
+  firstName: string;
+  surname: string;
+  middleName: string;
+  username: string;
+  password: string;
+  department: IDepartmentGet;
+  position: IPositionGet;
+  hireDate: String;
+  employmentType: string;
+  roles: string[];
+  address: object;
+  gender: string;
 }
 
 export interface IEmployeePost {
-    firstName: string;
-    surname: string;
-    middleName: string;
-    birthDate : string;
-    username: string;
-    password: string;
-    department: string;
-    position: string;
-    hireDate: string;
-    employmentType: string;
-    roles: string[];
-    address: object;
-    gender: string;
-  }
+  firstName: string;
+  surname: string;
+  middleName: string;
+  birthDate: string;
+  username: string;
+  password: string;
+  department: string;
+  position: string;
+  hireDate: String;
+  employmentType: string;
+  roles: string[];
+  address: object;
+  gender: string;
+}
