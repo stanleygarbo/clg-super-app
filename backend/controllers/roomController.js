f53c4433a57fb698b562dbb2ae9c7bfa575e853d
TechnoDev <74283450+TechnoX0@users.noreply.github.com>
2025-03-14 18:07:15 +0800

    updated room api

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -27,4 +27,37 @@ const getRooms = async (req, res) => {
   }
 };
 
-module.exports = { addRoom, getRooms };
+const updateRoom = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const room = await roomService.updateRoom({
+      id: req.params.id,
+      data: req.body,
+    });
+    res.status(201).json(room);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const deleteRoom = async (req, res) => {
+  try {
+    const room = await roomService.deleteRoom(req.params.id);
+    if (!room) {
+      return res.status(404).json({
+        message: "Not Found",
+      });
+    }
+
+    res.status(200).json(room);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = { addRoom, getRooms, updateRoom, deleteRoom };
--- frontend/src/api/room.ts
+++ frontend/src/api/room.ts
@@ -10,3 +10,13 @@ export const addRoom = async (room: IRoom) => {
   const response = await apiClient.post(`/rooms`, room);
   return response.data;
 }
+
+export const updateRoom = async (id: string) => {
+  const response = await apiClient.patch(`/rooms/${id}`)
+  return response.data
+}
+
+export const deleteRoom = async (id: string) => {
+  const response = await apiClient.delete(`/rooms/${id}`)
+  return response.data
+}
