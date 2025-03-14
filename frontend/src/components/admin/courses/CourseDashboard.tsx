d3437fa43e668895b15172738cb8c8c59d943dba
cripe101 <mhegryanlimpangog58@gmail.com>
2025-03-14 21:15:39 +0800

    New

--- frontend/src/components/admin/courses/CourseDashboard.tsx
+++ frontend/src/components/admin/courses/CourseDashboard.tsx
@@ -1,16 +1,10 @@
-import { MdOutlinePlaylistAdd } from "react-icons/md";
 import { useState } from "react";
 import { toast } from "react-toastify";
-import { courseData } from "../../../store/CourseData";
-import apiClient from "../../../api/apiClient";
-import AddCourse from "./AddCourse";
-import CoursesList from "./CoursesList";
 import { AiFillDelete } from "react-icons/ai";
 import { ICourseGet, ICoursePost } from "../../../interface/ICourse";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { addCourse, getCourses } from "../../../api/course";
+import { addCourse, deleteCourse, getCourses } from "../../../api/course";
 import { useForm } from "react-hook-form";
-import { IoHandLeft } from "react-icons/io5";
 
 const CourseDashboard = () => {
   const [search, setSearch] = useState<string>("");
@@ -25,6 +19,15 @@ const CourseDashboard = () => {
     mutationFn: addCourse,
     onSuccess: () => {
       toast.success("Added Successfully");
+      query.refetch();
+    },
+  });
+
+  const deleteCourMutation = useMutation({
+    mutationFn: deleteCourse,
+    onSuccess: () => {
+      toast.success("Deleted Successfully");
+      query.refetch();
     },
   });
 
@@ -35,32 +38,43 @@ const CourseDashboard = () => {
   return (
     <div>
       <div className="w-[1100px] h-[650px]">
-        {/* ADD DEPARTMENT */}
-        <section className={`p-5 flex items-center justify-between gap-10`}>
-          <h1 className="text-xl font-bold text-blue-800">Add Department</h1>
+        {/* ADD COURSE */}
+        <section className={`p-5 flex items-center justify-between gap-3`}>
+          <h1 className="text-xl font-bold text-blue-800 w-[120px]">
+            Add Course
+          </h1>
           <form
-            className="flex gap-10"
+            className="flex gap-3"
             onSubmit={handleSubmit((data: ICoursePost) =>
               addCourMutation.mutate(data)
             )}
           >
-            <section className="flex items-center gap-3">
-              <h1 className="font-semibold text-sm">Department Name :</h1>
-              <input
-                type="text"
-                // {...register("departmentName")}
-                placeholder="Department"
-                className="outline-none border-0 py-1 px-2 text-lg text-blue-900 font-semibold text-center border-b-2 border-b-blue-800"
-              />
-            </section>
+            <input
+              type="text"
+              {...register("courseCode")}
+              placeholder="Course Code"
+              className="outline-none border-0 py-1 px-2 text-lg text-blue-900 font-semibold text-center border-b-2 border-b-blue-800"
+            />
+            <input
+              type="text"
+              {...register("courseName")}
+              placeholder="Course Name"
+              className="outline-none border-0 py-1 px-2 text-lg text-blue-900 font-semibold text-center border-b-2 border-b-blue-800"
+            />
+            <input
+              type="number"
+              {...register("units")}
+              placeholder="Units"
+              className="outline-none border-0 py-1 px-2 text-lg text-blue-900 font-semibold text-center border-b-2 border-b-blue-800"
+            />
             <button
               type="submit"
-              className="bg-blue-600 w-[190px] flex justify-center py-2 text-white font-bold rounded-md hover:bg-blue-800 active:scale-95 duration-200"
+              className="bg-blue-600 w-[140px] flex justify-center py-2 text-white font-bold rounded-md hover:bg-blue-800 active:scale-95 duration-200"
             >
               {addCourMutation.isPending ? (
                 <img src="/loading.svg" className="invert" alt="" />
               ) : (
-                "Add Department"
+                "Add Course"
               )}
             </button>
           </form>
@@ -68,7 +82,7 @@ const CourseDashboard = () => {
 
         <section className="bg-slate-200 px-5 py-2 rounded-md flex items-center justify-between">
           <span className="flex gap-3">
-            <h1 className="text-xl font-bold text-blue-800">Department List</h1>
+            <h1 className="text-xl font-bold text-blue-800">Course's List</h1>
           </span>
           <span className="flex gap-3 ">
             <input
@@ -87,7 +101,7 @@ const CourseDashboard = () => {
             <span className="flex gap-5 mb-3 mt-2 text-lg">
               <h1 className="w-[150px] font-bold">Course Code</h1>
               <h1 className="w-[400px] font-bold text-center">Course</h1>
-              <h1 className="w-[200px] font-bold">Units</h1>
+              <h1 className="w-[100px] font-bold">Units</h1>
               <h1 className="w-[200px] font-bold text-center">Action</h1>
             </span>
             <span className="overflow-scroll no-scrollbar">
@@ -108,11 +122,11 @@ const CourseDashboard = () => {
                 >
                   <h1 className="w-[150px] pl-3">{cour.courseCode}</h1>
                   <h1 className="w-[400px] text-center">{cour.courseName}</h1>
-                  <h1 className="w-[200px]">{cour.units} units</h1>
+                  <h1 className="w-[100px]">{cour.units} units</h1>
                   <h1 className="w-[200px] flex justify-center">
                     <button
                       onClick={() => {
-                        // deleteDeptMutation.mutate(cour._id);
+                        deleteCourMutation.mutate(cour._id);
                       }}
                       className="bg-red-500 py-2 px-3 font-semibold text-xl text-white rounded-md hover:bg-red-700 active:scale-95 duration-200"
                     >
